768512923cc24faaf2d07e1d9397c9c0a640e009
Harshit <harshitsoni2806@gmail.com>
2022-09-07 08:28:56 +0530

    .

---
+++ src/main/java/leavecalculate/datalayer/User_Dao.java
@@ -0,0 +1,15 @@
+package leavecalculate.datalayer;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.stereotype.Repository;
+
+import leavecalculate.model.User;
+
+@Repository
+public class User_Dao {
+
+	
+}
