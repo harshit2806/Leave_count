9248b9ab87889df061faa0a511cb1073d8d158dd
Harshit <harshitsoni2806@gmail.com>
2022-09-11 18:33:14 +0530

    .

---
+++ src/main/java/leavecalculate/model/User_get.java
@@ -0,0 +1,17 @@
+package leavecalculate.model;
+
+public class User_get {
+
+	private int e_ID_get;
+	
+	private int e_ID_up;
+	
+	private int flx_lv_up;
+	
+	private int ern_lv_up;
+	
+	private int sik_lv_up;
+	
+	private int e_ID_del;
+
+}
