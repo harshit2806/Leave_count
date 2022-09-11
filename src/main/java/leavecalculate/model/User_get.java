7ded8fb5bdfa3992f687146379ef9e47425dd2e9
Harshit <harshitsoni2806@gmail.com>
2022-09-11 18:34:28 +0530

    .

--- src/main/java/leavecalculate/model/User_get.java
+++ src/main/java/leavecalculate/model/User_get.java
@@ -14,4 +14,33 @@ public class User_get {
 	
 	private int e_ID_del;
 
+	public int getE_ID_get() {
+		return e_ID_get;
+	}
+
+	public void setE_ID_get(int e_ID_get) {
+		this.e_ID_get = e_ID_get;
+	}
+
+	public int getE_ID_up() {
+		return e_ID_up;
+	}
+
+	public void setE_ID_up(int e_ID_up) {
+		this.e_ID_up = e_ID_up;
+	}
+
+	public int getFlx_lv_up() {
+		return flx_lv_up;
+	}
+
+	public void setFlx_lv_up(int flx_lv_up) {
+		this.flx_lv_up = flx_lv_up;
+	}
+
+	public int getErn_lv_up() {
+		return ern_lv_up;
+	}
+
+
 }
