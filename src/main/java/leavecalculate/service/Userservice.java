85cb4e71b05a32180b74d55c987825aca90a5ca8
Harshit <harshitsoni2806@gmail.com>
2022-09-13 19:39:02 +0530

    .

--- src/main/java/leavecalculate/service/Userservice.java
+++ src/main/java/leavecalculate/service/Userservice.java
@@ -1,7 +1,16 @@
 package leavecalculate.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
 
 @Service
 public class Userservice {
  
+	
+	public int Creatuser(User User)
+	{	
+	}
+
 }
