d6086b5cb7f5a3860dd262688a43bed087fa31fb
Harshit <harshitsoni2806@gmail.com>
2022-09-13 19:39:54 +0530

    .

--- src/main/java/leavecalculate/service/Userservice.java
+++ src/main/java/leavecalculate/service/Userservice.java
@@ -3,14 +3,18 @@ package leavecalculate.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
+import leavecalculate.datalayer.User_Dao;
+import leavecalculate.model.User;
+import leavecalculate.model.User_get;
 
 @Service
 public class Userservice {
  
-	
+	@Autowired
+	private User_Dao User_Dao;
 	public int Creatuser(User User)
-	{	
+	{
+		return this.User_Dao.Saveuser(User);	
 	}
 
 }
